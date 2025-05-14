2ff14bb827b26558ac3ed5465914fe9a66a2b050
Fabio Francisco <fabiofrancisco6002@gmail.com>
2025-05-14 11:39:15 +0700

    Perubahan nama header

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,7 +1,8 @@
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">Start Bootstrap</a>
+        <a class="navbar-brand" href="{{ url('/') }}">Fabio Francisco</a>
+        <a class="navbar-brand" href="{{ url('/') }}">535240077</a>
         <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
